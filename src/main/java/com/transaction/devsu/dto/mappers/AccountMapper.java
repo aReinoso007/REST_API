30d2adf8383c93e45d72beb982d2f10e1f718489
alex reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-07-11 00:15:05 -0500

    adding list conversion

--- src/main/java/com/transaction/devsu/dto/mappers/AccountMapper.java
+++ src/main/java/com/transaction/devsu/dto/mappers/AccountMapper.java
@@ -6,12 +6,19 @@ import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
     @Mapping(source = "accountNumber", target = "numeroCuenta")
     @Mapping(source = "accountType", target = "tipoCuenta")
     @Mapping(source = "initialBalance", target = "saldoInicial")
     @Mapping(source = "status", target = "estado")
+
+    @Mapping(source = "client.identification", target = "clienteCedula")
+    AccountDTO toAccountDTO(Account account);
+
+    List<AccountDTO> toAccountDTOs(List<Account> accounts);
 
     @InheritInverseConfiguration
     @Mapping(target = "id", ignore = true)
