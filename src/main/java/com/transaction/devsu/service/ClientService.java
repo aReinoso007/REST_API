144a5edf1d4f35feec83b790bd4714976fa83378
alex reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-07-11 01:15:00 -0500

    get all and exception

--- src/main/java/com/transaction/devsu/service/ClientService.java
+++ src/main/java/com/transaction/devsu/service/ClientService.java
@@ -1,15 +1,21 @@
 package com.transaction.devsu.service;
 
+import com.transaction.devsu.dto.ClientDTO;
 import com.transaction.devsu.dto.mappers.ClientMapper;
 import com.transaction.devsu.entities.Client;
 import com.transaction.devsu.repository.ClienteRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
+@Transactional
 public class ClientService {
 
     private final ClienteRepository clienteRepository;
@@ -21,6 +27,16 @@ public class ClientService {
         this.clientMapper = clientMapper;
     }
 
+    public List<ClientDTO> getAllClients(){
+        try{
+            Optional<List<Client>> clients = Optional.of(clienteRepository.findAll());
+            return clientMapper.toClientDTOs(clients.orElseGet(ArrayList::new));
+        }catch (Exception e){
+            log.error("Exception at ClientService while getting all");
+            throw new IllegalStateException(e.getMessage(), e);
+        }
+    }
+
     public void addNewCliente(Client client){
 
         Optional<Client> clienteOptional = clienteRepository.findClientByIdentification(client.getIdentification());
@@ -28,13 +44,6 @@ public class ClientService {
             throw new IllegalStateException("Client with the given identificacion has already been registered");
         }
         clienteRepository.save(client);
-    }
-
-    public List<Client> getClientes(){
-
-        List<Client> clients = clienteRepository.findAll();
-
-        return clients;
     }
 
 }
