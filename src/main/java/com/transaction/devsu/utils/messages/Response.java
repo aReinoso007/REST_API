998593d637e61bfc0c3aff28385b2e7bf065514c
alex reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-07-11 10:50:12 -0500

    http responses

--- src/main/java/com/transaction/devsu/utils/messages/Response.java
+++ src/main/java/com/transaction/devsu/utils/messages/Response.java
@@ -1,9 +1,25 @@
 package com.transaction.devsu.utils.messages;
 
+import org.springframework.http.HttpStatus;
+
+import java.math.BigDecimal;
+
 public class Response {
 
     public static final String CLIENT_EXISTS = "CLIENT ALREADY REGISTERED WITH PROVIDED CEDULA";
     public static final String CLIENT_NOT_FOUND ="CLIENT NOT REGISTERED";
+
+    /*200*/
+    public static final HttpStatus HTTP_STATUS_OK = HttpStatus.OK;
+    /*201*/
+    public static final HttpStatus HTTP_STATUS_CREATED = HttpStatus.CREATED;
+    /*400*/
+    public static final HttpStatus HTTP_STATUS_BAD_REQUEST = HttpStatus.BAD_REQUEST;
+    /*500*/
+    public static final HttpStatus HTTP_STATUS_INTERNAL_SERVER_ERROR = HttpStatus.INTERNAL_SERVER_ERROR;
+
+
+    public static final BigDecimal
 
 
 }
