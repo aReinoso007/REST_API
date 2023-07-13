d9c5143326edf7086dd6cf3776859455204a5f3e
alex reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-07-13 09:32:13 -0500

    reportdto

--- src/main/java/com/transaction/devsu/dto/Report.java
+++
@@ -1,5 +0,0 @@
-package com.transaction.devsu.dto;
-
-public interface Report {
-
-}
---
+++ src/main/java/com/transaction/devsu/dto/TransactionReportDTO.java
@@ -0,0 +1,15 @@
+package com.transaction.devsu.dto;
+
+import java.math.BigDecimal;
+
+public class TransactionReportDTO {
+
+    private String fecha;
+    private String cliente;
+    private String numeroCuenta;
+    private BigDecimal saldoInicial;
+    private Boolean estado;
+    private String movimiento;
+    private String saldoDisponible;
+
+}
