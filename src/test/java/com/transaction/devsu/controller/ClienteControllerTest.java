62e63a08de1c64f8cf9719bd15793173f1b7c404
alex reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-07-13 16:22:42 -0500

    save client controller inte test

--- src/test/java/com/transaction/devsu/controller/ClienteControllerTest.java
+++ src/test/java/com/transaction/devsu/controller/ClienteControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -36,10 +37,22 @@ class ClienteControllerTest {
     }
 
     @Test
-    void save() {
+    void save() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/clientes")
+        .content("{\n" +
+                "    \"cedula\":\"1400919406\",\n" +
+                "    \"nombre\":\"DEV SU TEST TRES\",\n" +
+                "    \"genero\":\"FEMALE\",\n" +
+                "    \"edad\":25,\n" +
+                "    \"direccion\":\"Ecuador\",\n" +
+                "    \"numeroTelefono\":\"0998952718\",\n" +
+                "    \"contrasena\":\"Abc123!df\",\n" +
+                "    \"estado\":true\n" +
+                "}").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(MockMvcResultMatchers
+                        .content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
     }
-
-    @Test
-    void deleteClientById() {
-    }
+    
 }
