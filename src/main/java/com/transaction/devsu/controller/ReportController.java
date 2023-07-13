c6e77532780ee4372d53eb9141a9cf81fa09a929
alex reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-07-13 10:22:19 -0500

    controller

--- src/main/java/com/transaction/devsu/controller/ReportController.java
+++ src/main/java/com/transaction/devsu/controller/ReportController.java
@@ -1,9 +1,15 @@
 package com.transaction.devsu.controller;
 
+import com.transaction.devsu.dto.TransactionReportDTO;
 import com.transaction.devsu.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/reportes")
@@ -14,6 +20,14 @@ public class ReportController {
     @Autowired
     public ReportController(ReportService reportService){
         this.reportService = reportService;
+    }
+
+    @GetMapping("")
+    public List<TransactionReportDTO> getReportByAccountNumberAndDateRange(@RequestParam String identification,
+                                                                           @RequestParam Date initDate,
+                                                                           @RequestParam Date endDate){
+        return reportService.geTransactionReportByIdentificationAndDateRange(identification, initDate, endDate);
+
     }
 
 }
