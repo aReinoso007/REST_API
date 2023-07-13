47ac904bbdc6289e0aff00eed98c2d274329d524
alex reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-07-13 09:36:27 -0500

    json format and prop assign to transrportdto

--- src/main/java/com/transaction/devsu/dto/TransactionReportDTO.java
+++ src/main/java/com/transaction/devsu/dto/TransactionReportDTO.java
@@ -1,15 +1,38 @@
 package com.transaction.devsu.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
-
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonPropertyOrder({
+        "fecha", "cliente","numeroCuenta",
+        "saldoInicial","estado","movimiento",
+        "saldoDisponible"
+})
 public class TransactionReportDTO {
-
+    @JsonProperty("Fecha")
+    @JsonFormat(pattern = "dd-MM-yyyy")
     private String fecha;
+    @JsonProperty("Cliente")
     private String cliente;
+    @JsonProperty("numeroCuenta")
     private String numeroCuenta;
+    @JsonProperty("SaldoInicial")
     private BigDecimal saldoInicial;
+    @JsonProperty("Estado")
     private Boolean estado;
+    @JsonProperty("Movimiento")
     private String movimiento;
+    @JsonProperty("SaldoDisponible")
     private String saldoDisponible;
 
 }
